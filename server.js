aafbccd39b2967a271b4132fd1c6cee9edfbbff3
FerchoKD <213524@ids.upchiapas.edu.mx>
2023-02-26 11:18:34 -0600

    chat funcional sin css

--- server.js
+++ server.js
@@ -4,51 +4,79 @@ const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const fs = require('fs');
 const globalUsers = new Set();
-
+const cambio = [];
+const socketsP = new Map();
 
 app.use(express.static(__dirname));
 
-app.get('/', function(req, res){
+app.get('/', function (req, res) {
   res.sendFile(__dirname + './index.html');
 });
 
-
-
-io.on('connection', function(socket){
+io.on('connection', function (socket) {
   console.log('Usuario conectado');
 
-  socket.on('disconnect', function(){
+  socket.on('disconnect', function () {
     console.log('Usuario desconectado');
   });
 
-  socket.on('set username', function(username){
+  socket.on('set username', function (username) {
     console.log('Usuario: ' + username);
+    socket.nicknames = username;
+    socketsP.set(socket.nicknames,socket.id);
   });
 
-  socket.on('chat message', function(msg){
+  socket.on('chat message', function (msg) {
     console.log('Mensaje: ' + msg);
     io.emit('chat message', msg);
   });
 
-  socket.on('chat image', function(data){
+  socket.on('chat image', function (data) {
     console.log('Imagen recibida');
     io.emit('chat image', data);
   });
+
+  socket.on('chat imagePrivate', function (data) {
+    console.log('Imagen recibida de froma privada xd');
+    //io.emit('chat image', data);
+     let receptor = data.receptor;
+      let emisor = socket.nicknames;
+      let img = data.img;
+      console.log(receptor)
+      console.log(emisor)
+      io.to(socketsP.get(receptor)).emit('chat imagePrivate',{emisor:emisor, img:img})
+      io.to(socketsP.get(emisor)).emit('chat imagePrivate',{emisor:emisor, img:img})
+  });
 });
+
+  
 
 var users = []; // lista de usuarios registrados
 
-io.on('connection', function(socket) {
+io.on('connection', function (socket) {
   // función para comprobar si un nombre de usuario ya existe en la lista de usuarios
   function usernameExists(username) {
     return users.indexOf(username) >= 0;
+
   }
 
-  socket.on('check username', function(username, callback) {
+  socket.on('messagePrivate',(data)=>{
+    let receptor = data.receptor;
+    let emisor = socket.nicknames;
+    let message = data.message;
+    console.log("entro");
+    console.log(receptor);
+    console.log(emisor);
+    console.log(message);
+    io.to(socketsP.get(receptor)).emit('newMessagePrivate',{emisor:emisor,message:message})
+    io.to(socketsP.get(emisor)).emit('newMessagePrivate',{emisor:emisor,message:message})
+  });
+
+  socket.on('check username', function (username, callback) {
     callback(usernameExists(username));
   });
 
-  socket.on('add user', function(username, callback) {
+  socket.on('add user', function (username, callback) {
     if (usernameExists(username)) {
       callback(false); // indicar que no se pudo agregar el usuario
       return;
@@ -63,6 +91,6 @@ io.on('connection', function(socket) {
 
 
 
-http.listen(3000, function(){
+http.listen(3000, function () {
   console.log('Servidor escuchando en el puerto 3000');
 });
